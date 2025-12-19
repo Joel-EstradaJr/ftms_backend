39c024d309a1089feec569b586ce45a311d5fd8d
Joel-EstradaJr <joelrestradajr@iskolarngbayan.pup.edu.ph>
2025-12-20 04:36:30 +0800

    Minor Updates

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Custom type declarations for CSS imports
- * This allows TypeScript to recognize CSS file imports
- */
-
-declare module '*.css';
-declare module '*.module.css';
-declare module '*.scss';
-declare module '*.module.scss';
